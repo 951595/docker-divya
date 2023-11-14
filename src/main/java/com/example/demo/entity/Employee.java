312f54a68b2fe1ea8d24d6e01454b3406bbbee86
951595 <gudurukrishn98@gmail.com>
2023-11-14 10:22:33 +0000

    first commit

---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,54 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String ename;
+	private int salary;
+	private String desg;
+	private String loction;
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public String getDesg() {
+		return desg;
+	}
+	public void setDesg(String desg) {
+		this.desg = desg;
+	}
+	public String getLoction() {
+		return loction;
+	}
+	public void setLoction(String loction) {
+		this.loction = loction;
+	}
+	
+	
+	
+
+}
